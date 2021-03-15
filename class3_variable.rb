5008bed362522b4a6b74099dd5483c99cda5f2a9
skmorshed75 <skmorshed75@yahoo.com>
2021-03-15 08:08:03 +0600

    class3 - Variables

---
+++ class3_variable.rb
@@ -0,0 +1,17 @@
+puts "There once was a man named George"
+puts "he was 70 years old"
+puts "he really liked the name George"
+puts "but didn't like being 70"
+
+character_name = "Jeal"
+character_age = "40"
+puts ("There once was a man named "+character_name)
+puts ("he was " + character_age + " years old")
+puts ("he really liked the name "+character_name)
+puts ("but didn't like being " + character_age)
+
+
+character_name = "Jack"
+character_age = "10"
+puts ("he really liked the name "+character_name)
+puts ("but didn't like being " + character_age)
