23df12aaf90660bb11b3aafa611b1487d18ceb8c
skmorshed75 <skmorshed75@yahoo.com>
2021-03-15 22:47:58 +0600

    class - 7 User Input

---
+++ class7_userinput.rb
@@ -0,0 +1,9 @@
+#puts "Enter your name :"
+#name = gets
+#puts ("Hello " + name)
+
+puts "Enter your name :"
+name = gets.chomp()
+puts "Enter your age :"
+age = gets.chomp()
+puts ("Hello " + name + " you are " + age)
