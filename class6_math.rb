4a193a1f195c17e045dbca56e9651101a3284df3
skmorshed75 <skmorshed75@yahoo.com>
2021-03-15 22:19:09 +0600

    class - 6 Math and Numbers

---
+++ class6_math.rb
@@ -0,0 +1,24 @@
+puts 5+9
+puts 5/10
+puts 5*10
+puts 5-10
+puts 10 % 3 #remainder
+
+num = 10
+puts ("My fav number is " + num.to_s) #to string
+
+num = -20
+puts num.abs() #Positive number
+
+num = 20.3154
+puts num.round() #20 nearest number
+
+num = 20.11
+puts num.ceil() #21 maximum number
+
+num = 20.21
+puts num.floor() #20 minimum number
+
+puts Math.sqrt(36) #6.0
+
+puts Math.log(1) #0.0
