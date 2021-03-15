17d9de191ddccc963221bd0719429ecd282b2a71
skmorshed75 <skmorshed75@yahoo.com>
2021-03-15 23:33:38 +0600

    class - 9 Building a Mad Libs Game

---
+++ class9_madlibsgame.rb
@@ -0,0 +1,13 @@
+
+puts "Enter a color :"
+color = gets.chomp()
+
+puts "Enter a plural Noun :"
+plural_noun = gets.chomp()
+
+puts "Enter a celebrity :"
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural_noun + " are Blue")
+puts ("I love " + celebrity)
