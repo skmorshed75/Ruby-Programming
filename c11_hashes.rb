8ba595e7c6c0d92e553a8b432d574fbe9ca943c1
skmorshed75 <skmorshed75@yahoo.com>
2021-03-16 06:55:09 +0600

    class - 11 Hashes

---
+++ c11_hashes.rb
@@ -0,0 +1,14 @@
+# Key, Value
+
+states = {
+  "Pennsilvania" => "PA", # or we can write :Pennsilvania => "PA"
+  #:Pennsilvania => "PA",
+  1 => "One",
+  "New York" => "NY",
+  "Michigan" => "MI"
+}
+
+#puts states
+#puts states["Michigan"]
+#puts states[:Pennsilvania]
+puts states[1]
