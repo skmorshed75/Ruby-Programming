95b0859a0160fb3ceda6d6e4ac47191c2516e510
skmorshed75 <skmorshed75@yahoo.com>
2021-03-16 23:15:08 +0600

    class - 13 Return Statement

---
+++ c13_returnstatement.rb
@@ -0,0 +1,18 @@
+def cube(num)
+  num * num * num
+end
+
+puts cube(3)
+
+
+def cube2(num)
+  return num * num * num
+  5
+end
+puts cube2(3)
+
+
+def cube3(num)
+  return num * num * num, 2
+end
+puts cube3(3)[1]
